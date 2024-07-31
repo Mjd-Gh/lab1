b4fb2714768dbc9fe5b36431c366b15ea64aae21
Mjd-Gh <Mjd.nasser7@gmail.com>
2024-07-31 11:40:32 +0300

    feature added

--- myapp/bin/myapp.dart
+++ myapp/bin/myapp.dart
@@ -2,4 +2,6 @@ import 'package:myapp/myapp.dart' as myapp;
 
 void main(List<String> arguments) {
   print('Hello world: ${myapp.calculate()}!');
+  // --- New feature added 
+  print("Hello from Mjd's branch");
 }
